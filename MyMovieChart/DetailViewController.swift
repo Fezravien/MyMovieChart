d583450f29ddeb450301b4ac6cbca0a720d3fa7b
pazrvien <yjaewoong@naver.com>
2020-10-28 00:31:43 +0900

    WebKit WebNavigationDelegate을 이용한 Delegate patten으로 통신 상태에 따른 처리

--- MyMovieChart/DetailViewController.swift
+++ MyMovieChart/DetailViewController.swift
@@ -11,11 +11,14 @@ import WebKit
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var wv: WKWebView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     var mvo: MovieVO! // 목록 화면에서 전달하는 영화 정보를 받을 변수
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("linkurl = \(mvo.detail!), title = \(mvo.title!)")
+        
+        self.wv.navigationDelegate = self
         
         // 네비게이션 바의 타이틀에 영화명을 출력한다.
         let navibar = self.navigationItem
@@ -29,4 +32,55 @@ class DetailViewController: UIViewController {
         self.wv.load(req)
     }
     
+}
+
+// MARK: - WKNavigationDelegate 프로토콜 구현
+extension DetailViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        spinner.startAnimating() // 인디케이터 뷰의 에니메이션을 실행
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        spinner.stopAnimating() // 인디케이터 뷰의 에니메이션을 중단
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        spinner.stopAnimating() // 인디케이터 뷰의 에니메이션 중지
+        
+        alert("상세 페이지를 읽어오지 못했습니다."){
+            // 버튼 클릭 시, 이전 화면으로 되돌려 보냄
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+        
+        // 오류시 경고창
+//        let alert = UIAlertController(title: "오류", message: "상세페이지를 읽어오지 못했습니다.", preferredStyle: .alert)
+//        let cancelAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+//            // 이전 화면으로 돌려보냄
+//            _ = self.navigationController?.popViewController(animated: true)
+//        }
+//        alert.addAction(cancelAction)
+//        self.present(alert, animated: false, completion: nil)
+    
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        spinner.stopAnimating()
+        alert("상세 페이지를 읽어오지 못했습니다."){
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
+
+// 오류시 경고창 간결하게 !
+// MARK: - 심플한 경고창 함수 정의용 익스텐션
+extension UIViewController {
+    func alert(_ message:String, onClick: (() -> Void)? = nil) {
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "OK", style: .cancel) { (_) in
+            onClick?()
+        })
+        DispatchQueue.main.async {
+            self.present(controller, animated: false, completion: nil)
+        }
+    }
 }
