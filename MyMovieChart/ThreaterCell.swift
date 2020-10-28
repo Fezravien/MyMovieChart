51e429a075ab795ffd6e5d467f1444b3741196b4
pazrvien <yjaewoong@naver.com>
2020-10-28 16:35:45 +0900

    Tapbar를 통한 영화관 정보 REST API로 영화관의 정보(이름, 지역, 전화번호) 가져와 tableview로 100개 보여주기

---
+++ MyMovieChart/ThreaterCell.swift
@@ -0,0 +1,16 @@
+//
+//  ThreaterCell.swift
+//  MyMovieChart
+//
+//  Created by 윤재웅 on 2020/10/28.
+//
+
+import UIKit
+
+class ThreaterCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var addr: UILabel!
+    @IBOutlet weak var tel: UILabel!
+    
+}
